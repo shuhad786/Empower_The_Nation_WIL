43787d1fbe0e55d4ea04e9fad4a10397efce6edd
shuhad786 <shuhad.loofer@gmail.com>
2025-10-14 21:34:48 +0200

    Create android enviroment setup

---
+++ Android/app/src/main/java/com/example/empowerthenationmobile/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.empowerthenationmobile
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.empowerthenationmobile.ui.theme.EmpowerTheNationMobileTheme
+
+class MainActivity : ComponentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
+    setContent {
+      EmpowerTheNationMobileTheme {
+
+      }
+    }
+  }
+}
+
+
+
+
+}
