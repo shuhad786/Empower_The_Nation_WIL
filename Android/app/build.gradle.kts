0d480981b91ec91fa44ca1ab7f7a5c362172fe3f
shuhad786 <shuhad.loofer@gmail.com>
2025-10-22 16:49:22 +0200

    Update targetSDK to 36 and compileSDK to 36

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
   namespace = "com.example.empowerthenationmobile"
-  compileSdk = 35
+  compileSdk = 36
 
   defaultConfig {
     applicationId = "com.example.empowerthenationmobile"
     minSdk = 24
-    targetSdk = 35
+    targetSdk = 36
     versionCode = 1
     versionName = "1.0"
 
