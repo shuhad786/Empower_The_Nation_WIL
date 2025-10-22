8a73080e7a9c9d4658ce18872a8b3dc9be2597b5
shuhad786 <shuhad.loofer@gmail.com>
2025-10-22 10:34:34 +0200

    Testing theme and color changes

--- Android/app/src/main/java/com/example/empowerthenationmobile/ui/theme/Color.kt
+++ Android/app/src/main/java/com/example/empowerthenationmobile/ui/theme/Color.kt
@@ -2,9 +2,9 @@ package com.example.empowerthenationmobile.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-// val GreenAccent = Color(0xFF3C4A1E)
-val GreenAccent = Color(0xFF4CAF50)  // Material Green 500, brighter green
-val OrangeText = Color(0xFFCB7318)
+// val GreenAccent = Color(0xFF3C4A1E) original green color
+val GreenAccent = Color(0xFF4CAF50)  // Material Green 500, brighter green test
+// val OrangeText = Color(0xFFCB7318)
 val BackgroundColor = Color(0xFFF8F0E0)
 val BrownAccent = Color(0xFF442F13)
 
