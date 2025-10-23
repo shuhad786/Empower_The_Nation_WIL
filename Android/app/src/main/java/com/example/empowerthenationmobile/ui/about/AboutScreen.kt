e1c70c4343dccad7864a6d6392108bd29d970bc8
trevorm10 <trevormoodley10@gmail.com>
2025-10-23 19:46:34 +0200

    added about and contact page to android

--- Android/app/src/main/java/com/example/empowerthenationmobile/ui/about/AboutScreen.kt
+++
@@ -1,9 +0,0 @@
-package com.example.empowerthenationmobile.ui.about
-
-import androidx.compose.runtime.Composable
-import androidx.compose.material3.Text
-
-@Composable
-fun AboutScreen() {
-  Text("About Screen")
-}
